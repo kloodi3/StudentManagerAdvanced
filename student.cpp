758559609142ad8cffbed7b23d37fc0ce5891e37
kloodi3 <kmalrashdi@mail.csuchico.edu>
2018-05-03 14:13:22 -0700

    File

---
+++ student.cpp
@@ -0,0 +1,43 @@
+#include "student.h" 
+#include <string> 
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+void student::setName(string firstInput, string lastInput) 
+{
+        last = lastInput; 
+        first = firstInput; 
+} 
+
+string student::fullName()
+{ 
+        first.append(" ");
+        first.append(last); 
+        return first;
+} 
+
+void student::addGrade(double input) 
+{
+        grades.push_back(input);      
+
+}
+
+double student::getScore()
+{   double finalGrade=0;
+        double number=0; 
+        for(int i = 0 ; i<grades.size(); i++)
+        {
+                number = number + grades[i]; 
+        } 
+        if(grades.size() !=0)
+        {
+                finalGrade = number/grades.size(); 
+                return finalGrade; 
+        } 
+        else 
+        { 
+                return finalGrade = 0; 
+
+        }
+} 
---
+++ student.h
@@ -0,0 +1,23 @@
+#ifndef STUDENT_H 
+#define STUDENT_H 
+#include <string>
+#include <iostream> 
+#include <vector>
+
+class student 
+{ 
+        private:
+                std::string last; 
+                std::string first; 
+                std::vector< double > grades; 
+
+        public:
+                std::string fullName();      
+                void setName(std::string, std::string); 
+                double getScore(); 
+                void addGrade(double); 
+
+}; 
+
+
+#endif
